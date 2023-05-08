4930e39e370792b7907b5a9886bac298c778c990
Vorple-Sky <hannahg3211@gmail.com>
2023-05-08 09:36:13 -0700

    Code comments

--- sketch.js
+++ sketch.js
@@ -1,28 +1,10 @@
-/***********************************************************************************
-  Project 2
-  by Hannah Gabany
-
-  Uses the p5.2DAdventure.js class 
-  
-------------------------------------------------------------------------------------
-	To use:
-	Add this line to the index.html
-
-  <script src="p5.2DAdventure.js"></script>
-***********************************************************************************/
-
-//--- TEMPLATE STUFF: Don't change
-
-// adventure manager global
-
 var adventureManager;
 
-// p5.play
+
 var playerAvatar;
 
-// Clickables: the manager class
-var clickablesManager;    // the manager class
-var clickables;           // an array of clickable objects
+var clickablesManager;    
+var clickables;           
 
 // keycods for W-A-S-D
 const W_KEY = 87;
@@ -30,9 +12,7 @@ const S_KEY = 83;
 const D_KEY = 68;
 const A_KEY = 65;
 
-//---
 
-//-- MODIFY THIS for different speeds
 var speed = 5;
 
 //--- Your globals would go here
@@ -40,7 +20,7 @@ var speed = 5;
 
 // Allocate Adventure Manager with states table and interaction tables
 function preload() {
-  //--- TEMPLATE STUFF: Don't change
+ 
   clickablesManager = new ClickableManager('data/clickableLayout.csv');
   adventureManager = new AdventureManager('data/adventureStates.csv', 'data/interactionTable.csv', 'data/clickableLayout.csv');
   //---
@@ -50,22 +30,22 @@ function preload() {
 function setup() {
   createCanvas(1280, 720);
 
-  //--- TEMPLATE STUFF: Don't change
+  
   // setup the clickables = this will allocate the array
   clickables = clickablesManager.setup();
-  //---
+ 
 
-  // MODIFY THIS: change to initial position
+  
   playerAvatar = new Avatar("Player", 640, 400);
    
-  // MODIFY THIS: to make your avatar go faster or slower
-  playerAvatar.setMaxSpeed(10);
+  
+  playerAvatar.setMaxSpeed(8);
 
-  // MODIFY THIS: add your filenames here, right now our moving animation and standing animation are the same
+  
   playerAvatar.addMovingAnimation( 'assets/BrainWalkingforward01_.png', 'assets/BrainWalkingforward02.png');
   playerAvatar.addStandingAnimation('assets/BrainStandingright01_.png');
 
-  //--- TEMPLATE STUFF: Don't change
+  
   // use this to track movement from toom to room in adventureManager.draw()
   adventureManager.setPlayerSprite(playerAvatar.sprite);
 
@@ -79,15 +59,13 @@ function setup() {
   // call OUR function to setup additional information about the p5.clickables
   // that are not in the array 
   setupClickables(); 
-  //--
-
-  // NEW: line for debugging
+  
   //adventureManager.changeState("Unlucky");
 }
 
-// Adventure manager handles it all!
+
 function draw() {
-  //--- TEMPLATE STUFF: Don't change
+
   // draws background rooms and handles movement from one to another
 
 
@@ -98,18 +76,18 @@ function draw() {
   clickablesManager.draw();
   //---
 
-  //--- MODIFY THESE CONDITONALS
+  
   // No avatar for Splash screen or Instructions screen
   if( adventureManager.getStateName() !== "Splash" && 
       adventureManager.getStateName() !== "Instructions" ) {
       
-    //--- TEMPLATE STUFF: Don't change    
+       
     // responds to keydowns
     checkMovement();
 
-    // this is a function of p5.play, not of this sketch
+    
     drawSprite(playerAvatar.sprite);
-    //--
+    
   } 
 }
 
@@ -120,8 +98,7 @@ function keyPressed() {
 
 
 
-//--- TEMPLATE STUFF: Don't change 
-// respond to W-A-S-D or the arrow keys
+
 function checkMovement() {
   var xSpeed = 0;
   var ySpeed = 0;
@@ -144,10 +121,8 @@ function checkMovement() {
 
   playerAvatar.setSpeed(xSpeed,ySpeed);
 }
-//--
 
-//-- MODIFY THIS: this is an example of how I structured my code. You may
-// want to do it differently
+
 function mouseReleased() {
   if( adventureManager.getStateName() === "Splash") {
     adventureManager.changeState("Instructions");
@@ -157,7 +132,7 @@ function mouseReleased() {
 
 //-------------- CLICKABLE CODE  ---------------//
 
-//--- TEMPLATE STUFF: Don't change 
+
 function setupClickables() {
   // All clickables to have same effects
   for( let i = 0; i < clickables.length; i++ ) {
@@ -166,9 +141,7 @@ function setupClickables() {
     clickables[i].onPress = clickableButtonPressed;
   }
 }
-//--
 
-//-- MODIFY THIS:
 // tint when mouse is over
 clickableButtonHover = function () {
   this.color = "#AA33AA";
@@ -176,17 +149,15 @@ clickableButtonHover = function () {
   this.tint = "#FF0000";
 }
 
-//-- MODIFY THIS:
+
 // color a light gray if off
 clickableButtonOnOutside = function () {
   // backto our gray color
   this.color = "#AAAAAA";
 }
 
-//--- TEMPLATE STUFF: Don't change 
+
 clickableButtonPressed = function() {
-  // these clickables are ones that change your state
-  // so they route to the adventure manager to do this
   adventureManager.clickablePressed(this.name); 
 }
 //
@@ -195,32 +166,21 @@ clickableButtonPressed = function() {
 
 //-------------- SUBCLASSES / YOUR DRAW CODE CAN GO HERE ---------------//
 
-//-- MODIFY THIS:
-// Change for your own instructions screen
-
-// Instructions screen has a backgrounnd image, loaded from the adventureStates table
-// It is sublcassed from PNGRoom, which means all the loading, unloading and drawing of that
-// class can be used. We call super() to call the super class's function as needed
 class InstructionsScreen extends PNGRoom {
-  // preload is where we define OUR variables
-  // Best not to use constructor() functions for sublcasses of PNGRoom
-  // AdventureManager calls preload() one time, during startup
+ 
   preload() {
-    // These are out variables in the InstructionsScreen class
+    
     this.textBoxWidth = (width/6)*4;
     this.textBoxHeight = (height/6)*4; 
 
-    // hard-coded, but this could be loaded from a file if we wanted to be more elegant
-    //this.instructionsText = "You are navigating through the interior space of your moods. There is no goal to this game, but just a chance to explore various things that might be going on in your head. Use the ARROW keys to navigate your avatar around.";
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our instructions on top of this
+
   draw() {
     // tint down background image so text is more readable
     //tint(128);
      
-    // this calls PNGRoom.draw()
+    
     super.draw();
       
     // text draw settings
@@ -233,8 +193,7 @@ class InstructionsScreen extends PNGRoom {
   }
 }
 
-//-- MODIFY THIS: for your own classes
-// NEW: NPCClass
+
 class NPCRoom extends PNGRoom {
   preload() {
     // define class varibles here, load images or anything else
@@ -245,8 +204,7 @@ class NPCRoom extends PNGRoom {
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+ 
   draw() {
     // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
     if( this.hasSetup === false ) {
@@ -259,15 +217,13 @@ class NPCRoom extends PNGRoom {
       this.hasSetup = true; 
     }
 
-    // this calls PNGRoom.draw()
     super.draw();
 
     // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -284,10 +240,9 @@ class NPCRoom1 extends PNGRoom {
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+ 
   draw() {
-    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    
     if( this.hasSetup === false ) {
       // setup NPC 1
       this.npc1.setup();
@@ -298,15 +253,14 @@ class NPCRoom1 extends PNGRoom {
       this.hasSetup = true; 
     }
 
-    // this calls PNGRoom.draw()
+    
     super.draw();
 
     // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -325,10 +279,9 @@ class NPCRoom2 extends PNGRoom {
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+  
   draw() {
-    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    
     if( this.hasSetup === false ) {
       // setup NPC 1
       this.npc1.setup();
@@ -342,12 +295,10 @@ class NPCRoom2 extends PNGRoom {
     // this calls PNGRoom.draw()
     super.draw();
 
-    // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -366,12 +317,10 @@ class NPCRoom3 extends PNGRoom {
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+  
   draw() {
-    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    
     if( this.hasSetup === false ) {
-      // setup NPC 1
       this.npc1.setup();
       this.npc1.setPromptLocation(0,-30);
       
@@ -380,15 +329,14 @@ class NPCRoom3 extends PNGRoom {
       this.hasSetup = true; 
     }
 
-    // this calls PNGRoom.draw()
+    
     super.draw();
 
     // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -399,18 +347,15 @@ class NPCRoom3 extends PNGRoom {
 
 class NPCRoom4 extends PNGRoom {
   preload() {
-    // define class varibles here, load images or anything else
     this.npc1 = new NPC("Boss", 400, 570, 'assets/Npc2.png');
     this.npc1.addSingleInteraction("Brain, your late again \n I\'m not happy about this.");
     
-    // setup flag, seto false
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+
   draw() {
-    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    
     if( this.hasSetup === false ) {
       // setup NPC 1
       this.npc1.setup();
@@ -421,15 +366,14 @@ class NPCRoom4 extends PNGRoom {
       this.hasSetup = true; 
     }
 
-    // this calls PNGRoom.draw()
+    
     super.draw();
 
     // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -439,7 +383,7 @@ class NPCRoom4 extends PNGRoom {
 }
 class NPCRoom5 extends PNGRoom {
   preload() {
-    // define class varibles here, load images or anything else
+   
     this.npc1 = new NPC("Family", 230, 520, 'assets/Npc3.png');
     this.npc1.addSingleInteraction("Brain, honey I\'m glad to see you heading home \n I\'m happy you are safe.");
     
@@ -447,10 +391,9 @@ class NPCRoom5 extends PNGRoom {
     this.hasSetup = false;
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+ 
   draw() {
-    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    
     if( this.hasSetup === false ) {
       // setup NPC 1
       this.npc1.setup();
@@ -461,15 +404,14 @@ class NPCRoom5 extends PNGRoom {
       this.hasSetup = true; 
     }
 
-    // this calls PNGRoom.draw()
+    
     super.draw();
 
     // draw our NPCs
     drawSprite(this.npc1.sprite);
     
 
-    // When you have multiple NPCs, you can add them to an array and have a function 
-    // iterate through it to call this function for each more concisely.
+    
     this.npc1.displayInteractPrompt(playerAvatar);
     
   }
@@ -477,24 +419,19 @@ class NPCRoom5 extends PNGRoom {
   
 
 }
-//-- MODIFY THIS: for your own classes
-// (1) copy this code block below
-// (2) paste after //-- done copy
-// (3) Change name of TemplateScreen to something more descriptive, e.g. "PuzzleRoom"
-// (4) Add that name to the adventureStates.csv file for the classname for that appropriate room
+
 class TemplateScreen extends PNGRoom {
   preload() {
-    // define class varibles here, load images or anything else
+    
   }
 
-  // call the PNGRoom superclass's draw function to draw the background image
-  // and draw our code adter this
+  
   draw() {
-    // this calls PNGRoom.draw()
+    
     super.draw();
 
-    // Add your code here
+  
   }
 }
-//-- done copy
+
 
